a1a8f2ca49f551c77ff956fb457a335242c3b91c
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2017-03-16 11:30:22 +0100

    initial

--- src/Providers/CodeCampThemePluginServiceProvider.php
+++ src/Providers/CodeCampThemePluginServiceProvider.php
@@ -3,24 +3,11 @@
 namespace CodeCampThemePlugin\Providers;
 
 use Plenty\Plugin\ServiceProvider;
-use Plenty\Plugin\Events\Dispatcher;
-use Plenty\Plugin\Templates\Twig;
-use IO\Helper\TemplateContainer;
 
 class CodeCampThemePluginServiceProvider extends ServiceProvider
 {
-    const EVENT_LISTENER_PRIORITY = 1;
-
     /**
      * Register the service provider.
      */
     public function register() {}
-
-    public function boot(Twig $twig, Dispatcher $eventDispatcher)
-    {
-        $eventDispatcher->listen('tpl.basket', function (TemplateContainer $templateContainer)
-        {
-            $templateContainer->setTemplate('CodeCampThemePlugin::content.CodeCampThemePluginBasket');
-        }, self::EVENT_LISTENER_PRIORITY);
-    }
 }
